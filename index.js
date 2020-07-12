e129661c8ccf2b5ee01abeedc72c8af158fb9c9b
JaiminPatel <jaimin.patel@oneadvanced.com>
2020-07-13 00:25:26 +0530

    adding ddb

--- index.js
+++ index.js
@@ -9,8 +9,9 @@ async function run() {
     const key = core.getInput('key');
     const secret = core.getInput('secret');
     const repo = core.getInput('repository');
-    process.env['aws_access_key_id'] = key;
-    process.env['aws_secret_access_key'] = secret;
+    process.env['AWS_ACCESS_KEY_ID'] = key;
+    process.env['AWS_SECRET_ACCESS_KEY'] = secret;
+    process.env['AWS_DEFAULT_REGION'] = 'ap-southeast-1';
     console.log(`Creating record for ${repo} at ${new Date()}`)
     console.log('Github context : %j', github.context)
 
