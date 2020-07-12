b2f05610858edc8a43e8d9f93e822fa756639c6e
JaiminPatel <jaimin.patel@oneadvanced.com>
2020-07-13 00:27:54 +0530

    adding ddb

--- ddb.js
+++ ddb.js
@@ -1,6 +1,6 @@
 var AWS = require("aws-sdk");
 const github = require("@actions/github")
-AWS.config.update({ region: 'ap-southeast-1' });
+AWS.config.update({ region: process.env['AWS_DEFAULT_REGION'] });
 var docClient = new AWS.DynamoDB.DocumentClient();
 
 let makeEntry = async function(key, secret, repo) {
