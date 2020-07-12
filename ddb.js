c849964dffdcafcb54f10783868c5994953a8147
JaiminPatel <jaimin.patel@oneadvanced.com>
2020-07-12 23:02:05 +0530

    adding github context

--- ddb.js
+++ ddb.js
@@ -4,15 +4,15 @@ AWS.config.update({ region: process.env.AWS_REGION });
 var ddb = new AWS.DynamoDB({ apiVersion: "2012-08-10" });
 var docClient = new AWS.DynamoDB.DocumentClient();
 
-let makeEntry = function(table, key, secret) {
-  const Item = {
-    TableName: table
-  }
+let makeEntry = function(milliseconds) {
+  // const Item = {
+  //   TableName: table
+  // }
   return new Promise((resolve, reject) => {
     if (typeof(milliseconds) !== 'number') { 
       throw new Error('milleseconds not a number'); 
     }
-    console.log('github context : %j',github.context)
+    // console.log('github context : %j',github.context)
 
     setTimeout(() => resolve("done!"), milliseconds)
   });
