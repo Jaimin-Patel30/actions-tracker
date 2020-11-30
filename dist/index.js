85bb85b450105bb048f42705574c54b38800c762
JaiminPatel <jaimin.patel@oneadvanced.com>
2020-11-30 17:31:57 +0530

    testing actions

--- dist/index.js
+++ dist/index.js
@@ -404,11 +404,7 @@ const { readdirSync } = __webpack_require__(747)
 // most @actions toolkit packages have async methods
 async function run () {
   try {
-    const key = core.getInput('key')
-    const secret = core.getInput('secret')
     const repo = core.getInput('repository')
-    process.env.AWS_ACCESS_KEY_ID = key
-    process.env.AWS_SECRET_ACCESS_KEY = secret
     process.env.AWS_DEFAULT_REGION = 'ap-southeast-1'
     console.log(`Creating record for ${repo} at ${new Date()}`)
     console.log('Github context : %j', github.context)
--- index.js
+++ index.js
@@ -5,11 +5,7 @@ const { readdirSync } = require('fs')
 // most @actions toolkit packages have async methods
 async function run () {
   try {
-    const key = core.getInput('key')
-    const secret = core.getInput('secret')
     const repo = core.getInput('repository')
-    process.env.AWS_ACCESS_KEY_ID = key
-    process.env.AWS_SECRET_ACCESS_KEY = secret
     process.env.AWS_DEFAULT_REGION = 'ap-southeast-1'
     console.log(`Creating record for ${repo} at ${new Date()}`)
     console.log('Github context : %j', github.context)
