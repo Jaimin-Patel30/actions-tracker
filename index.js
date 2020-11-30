ede298b068bff673dc5628aa8b67f0cf52b44142
JaiminPatel <jaimin.patel@oneadvanced.com>
2020-11-30 17:27:26 +0530

    testing actions

--- index.js
+++ index.js
@@ -1,28 +1,23 @@
-const core = require('@actions/core');
+const core = require('@actions/core')
 const github = require('@actions/github')
-const makeEntry = require('./ddb');
-
+const { readdirSync } = require('fs')
 
 // most @actions toolkit packages have async methods
-async function run() {
-  try { 
-    const key = core.getInput('key');
-    const secret = core.getInput('secret');
-    const repo = core.getInput('repository');
-    process.env['AWS_ACCESS_KEY_ID'] = key;
-    process.env['AWS_SECRET_ACCESS_KEY'] = secret;
-    process.env['AWS_DEFAULT_REGION'] = 'ap-southeast-1';
+async function run () {
+  try {
+    const key = core.getInput('key')
+    const secret = core.getInput('secret')
+    const repo = core.getInput('repository')
+    process.env.AWS_ACCESS_KEY_ID = key
+    process.env.AWS_SECRET_ACCESS_KEY = secret
+    process.env.AWS_DEFAULT_REGION = 'ap-southeast-1'
     console.log(`Creating record for ${repo} at ${new Date()}`)
     console.log('Github context : %j', github.context)
-
-    core.debug((new Date()).toTimeString())
-    await makeEntry(key, secret, repo);
-    core.debug((new Date()).toTimeString())
-
-    core.setOutput('time', new Date().toTimeString());
-  } 
-  catch (error) {
-    core.setFailed(error.message);
+    const dirs = readdirSync('./', { withFileTypes: true })
+    console.log(dirs)
+    core.setOutput('time', new Date().toTimeString())
+  } catch (error) {
+    core.setFailed(error.message)
   }
 }
 
